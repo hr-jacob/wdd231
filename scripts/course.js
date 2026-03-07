608159f507d85ebb5eeaaa2e35c8b28a07fa02e7
hr-jacob <jherreraroca@byupathway.edu>
2026-03-07 14:27:53 -0600

    js

---
+++ scripts/course.js
@@ -0,0 +1,48 @@
+const courses = [
+{code:"WDD130", credits:3, completed:true},
+{code:"WDD131", credits:3, completed:false},
+{code:"WDD231", credits:3, completed:false},
+{code:"CSE110", credits:2, completed:true},
+{code:"CSE111", credits:2, completed:false}
+];
+
+const container = document.querySelector("#courses");
+const credits = document.querySelector("#credits");
+
+function displayCourses(list){
+
+container.innerHTML="";
+
+list.forEach(course => {
+
+const card = document.createElement("div");
+
+card.textContent = course.code;
+
+if(course.completed){
+card.classList.add("completed");
+}
+
+container.appendChild(card);
+
+});
+
+const total = list.reduce((sum, course)=> sum + course.credits,0);
+
+credits.textContent = total;
+
+}
+
+displayCourses(courses);
+
+document.querySelector("#all").addEventListener("click", ()=>{
+displayCourses(courses);
+});
+
+document.querySelector("#wdd").addEventListener("click", ()=>{
+displayCourses(courses.filter(c => c.code.includes("WDD")));
+});
+
+document.querySelector("#cse").addEventListener("click", ()=>{
+displayCourses(courses.filter(c => c.code.includes("CSE")));
+});
