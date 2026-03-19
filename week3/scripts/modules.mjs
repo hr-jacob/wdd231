04c9009a9278d5f9c5f6bbd8402e219993e1433f
hr-jacob <jherreraroca@byupathway.edu>
2026-03-19 16:59:04 -0600

    sections

--- week3/scripts/modules.mjs
+++ week3/scripts/modules.mjs
@@ -1,14 +1,3 @@
-
-function setSectionSelection() {
-  const sectionSelect = document.querySelector("#sectionNumber");
-  byuiCourse.sections.forEach((section) => {
-    const option = document.createElement("option");
-    option.value = section.sectionNumber;
-    option.textContent = `${section.sectionNumber}`;
-    sectionSelect.appendChild(option);
-  });
-}
-
 function setTitle(course) {
   document.querySelector("#courseName").textContent = course.name;
   document.querySelector("#courseCode").textContent = course.code;
