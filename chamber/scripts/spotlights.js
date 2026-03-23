96c9764961d76538795cce312cb3c4671e6e9138
hr-jacob <jherreraroca@byupathway.edu>
2026-03-23 17:27:27 -0600

    logo

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -36,7 +36,7 @@ function displaySpotlights(members) {
 
     card.innerHTML = `
       <h3>${member.name}</h3>
-      <img src="${member.image}" alt="${member.name} logo" loading="lazy">
+      <img src="images/${member.image}" alt="${member.name} logo" loading="lazy">
       <p><strong>Phone:</strong> ${member.phone}</p>
       <p><strong>Address:</strong> ${member.address}</p>
       <p><strong>Membership:</strong> ${getMembershipLevel(member.membership)}</p>
