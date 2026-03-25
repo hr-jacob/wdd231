8179b42eca6d7b5ebe91b5cfa87db1b7798cb90b
hr-jacob <jherreraroca@byupathway.edu>
2026-03-25 09:08:45 -0600

    function

--- week3/scripts/modules.js
+++ week3/scripts/modules.js
@@ -25,3 +25,21 @@ document.querySelector("#dropStudent").addEventListener("click", function () {
 setTitle(byuiCourse);
 setSectionSelection(byuiCourse.sections);
 renderSections(byuiCourse.sections);
+
+function displayCourseDetails(course) {
+  courseDetails.innerHTML = '';
+  courseDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits</strong>: ${course.credits}</p>
+    <p><strong>Certificate</strong>: ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+  `;
+  courseDetails.showModal();
+  
+  closeModal.addEventListener("click", () => {
+    courseDetails.close();
+  });
+}
